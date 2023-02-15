e1b34edfb84a53caedc93ee95b22beada5370b42
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-02-15 13:24:07 -0300

    Update create-model.py
    Normalizar os dados: Normalizar os dados de entrada para que todas as jogadas tenham a mesma escala. É possível usar o MinMaxScaler do scikit-learn para isso.
    
    Dividir os dados em conjuntos de treinamento e teste: Dividir os dados em conjuntos de treinamento e teste para avaliar o desempenho do modelo em dados que nunca foram vistos durante o treinamento. É possível usar o train_test_split do scikit-learn para isso.
    
    Usar early stopping: Configurar o treinamento do modelo para usar early stopping para evitar overfitting. É possível usar a callback EarlyStopping do Keras para isso.
    
    Aumentar o número de épocas: Aumentar o número de épocas de treinamento para permitir que o modelo tenha mais tempo para aprender a partir dos dados.
    
    Reduzir o tamanho do batch: Reduzir o tamanho do batch para evitar que o modelo fique preso em mínimos locais.

--- model/create-model.py
+++ model/create-model.py
@@ -2,7 +2,10 @@ import numpy as np
 import json
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from keras.callbacks import EarlyStopping
 from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 # Carregar os dados do arquivo jsonl
 data = []
@@ -16,13 +19,21 @@ for item in data:
     X.append([int(x) for x in item["input"].split(',')])
     y.append(int(item["output"]))
 
+# Normalizar os dados de entrada
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+# Dividir os dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Calcular pesos de classe
 class_weights = {0: 1/0.067, 1: 1/0.467, 2: 1/0.467}
 
 # Preparar os dados para o treinamento
-X = np.array(X)
-X = np.reshape(X, (len(X), len(X[0]), 1))
-y = np_utils.to_categorical(y)
+X_train = np.reshape(X_train, (len(X_train), len(X_train[0]), 1))
+X_test = np.reshape(X_test, (len(X_test), len(X_test[0]), 1))
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
 
 # Criar o modelo LSTM
 model = Sequential()
@@ -30,8 +41,12 @@ model.add(LSTM(32, input_shape=(len(X[0]), 1)))
 model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Usar early stopping durante o treinamento
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+
 # Treinar o modelo
-model.fit(X, y, epochs=10, batch_size=32, class_weight=class_weights)
+model.fit(X_train, y_train, epochs=50, batch_size=8, class_weight=class_weights, 
+          validation_data=(X_test, y_test), callbacks=[early_stopping])
 
 # Salvar o modelo em disco
 model.save("model.h5")
