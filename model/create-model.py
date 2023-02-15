ba3c13ff71c884171a2f3736c32f9add4465d67a
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-02-15 11:10:02 -0300

    Update create-model.py

--- model/create-model.py
+++ model/create-model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from keras.layers import Dense, LSTM
 from keras.utils import np_utils
 
 # Carregar os dados do arquivo jsonl
@@ -13,31 +13,25 @@ with open("data.jsonl") as f:
 # Separar os dados em entrada (X) e saída (y)
 X, y = [], []
 for item in data:
-    input_data = [int(x) for x in item["input"].split(',')]
-    output_data = int(item["output"])
-    if output_data == 0:
-        # Transforma o output 0 em um array de duas posições, para indicar que a jogada foi perdedora
-        y.append([0, 1])
-    else:
-        # Transforma o output 1 ou 2 em um array de duas posições, para indicar que a jogada foi vencedora
-        y.append([1, 0])
-    X.append(input_data)
+    X.append([int(x) for x in item["input"].split(',')])
+    y.append(int(item["output"]))
+
+# Calcular pesos de classe
+class_weights = {0: 1/0.067, 1: 1/0.467, 2: 1/0.467}
 
 # Preparar os dados para o treinamento
 X = np.array(X)
 X = np.reshape(X, (len(X), len(X[0]), 1))
-y = np.array(y)
+y = np_utils.to_categorical(y)
 
 # Criar o modelo LSTM
 model = Sequential()
 model.add(LSTM(32, input_shape=(len(X[0]), 1)))
-model.add(Dropout(0.2))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Treinar o modelo
-model.fit(X, y, epochs=10, batch_size=32)
+model.fit(X, y, epochs=10, batch_size=32, class_weight=class_weights)
 
 # Salvar o modelo em disco
 model.save("model.h5")
