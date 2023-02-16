deae94fddd0f339120b6781712f1f067ae0babab
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-02-16 00:15:17 -0300

    Update create-model.py

--- model/create-model.py
+++ model/create-model.py
@@ -1,52 +1,64 @@
-import numpy as np
 import json
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
-from keras.callbacks import EarlyStopping
-from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
-# Carregar os dados do arquivo jsonl
+# Carregando o dataset
 data = []
-with open("data.jsonl") as f:
+with open("novo_arquivo.jsonl", "r") as f:
     for line in f:
         data.append(json.loads(line))
 
-# Separar os dados em entrada (X) e saída (y)
-X, y = [], []
-for item in data:
-    X.append([int(x) for x in item["input"].split(',')])
-    y.append(int(item["output"]))
+# Separando os inputs e outputs do dataset
+inputs = []
+outputs = []
+for example in data:
+    inputs.append([int(i) for i in example["input"].split(",")])
+    outputs.append(int(example["output"]))
 
-# Normalizar os dados de entrada
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
+# Convertendo os inputs e outputs em arrays numpy
+inputs = np.array(inputs)
+outputs = np.array(outputs)
 
-# Dividir os dados em conjuntos de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Definindo as probabilidades de sair para cada cor
+probabilities = {0: 0.0666, 1: 0.4666, 2: 0.4666}
 
-# Calcular pesos de classe
-class_weights = {0: 1/0.067, 1: 1/0.467, 2: 1/0.467}
+# Definindo os pontos para cada cor
+points = {0: 14, 1: 1, 2: 1}
 
-# Preparar os dados para o treinamento
-X_train = np.reshape(X_train, (len(X_train), len(X_train[0]), 1))
-X_test = np.reshape(X_test, (len(X_test), len(X_test[0]), 1))
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+# Atribuindo valores para as classes de saída
+class_mapping = {0: 1, 1: 1, 2: 2}
+outputs_mapped = np.vectorize(class_mapping.get)(outputs)
 
-# Criar o modelo LSTM
+# Dividindo o dataset em treino e teste
+train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(inputs, outputs_mapped, test_size=0.2)
+
+# Normalizando os dados de entrada
+mean = train_inputs.mean(axis=0)
+std = train_inputs.std(axis=0)
+train_inputs = (train_inputs - mean) / std
+test_inputs = (test_inputs - mean) / std
+
+# Calculando os pesos de classe
+class_weights = {}
+for i in range(3):
+    class_weights[i] = (points[i] / probabilities[i]) / sum([points[j] / probabilities[j] for j in range(3)])
+
+
+# Criando o modelo de rede neural recorrente
 model = Sequential()
-model.add(LSTM(32, input_shape=(len(X[0]), 1)))
+model.add(LSTM(32, input_shape=(train_inputs.shape[1], 1)))
 model.add(Dense(3, activation='softmax'))
+
+# Compilando o modelo
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Usar early stopping durante o treinamento
-early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+# Treinando o modelo com os pesos de classe definidos
+model.fit(train_inputs[:, :, np.newaxis], np.eye(3)[train_outputs-1], epochs=10, batch_size=32, class_weight=class_weights)
 
-# Treinar o modelo
-model.fit(X_train, y_train, epochs=50, batch_size=8, class_weight=class_weights, 
-          validation_data=(X_test, y_test), callbacks=[early_stopping])
+# Avaliando o modelo no conjunto de testes
+score = model.evaluate(test_inputs[:, :, np.newaxis], np.eye(3)[test_outputs-1])
+print(f"Test accuracy: {score[1]}")
 
-# Salvar o modelo em disco
-model.save("model.h5")
+model.save("modelo.h5")
